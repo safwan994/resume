31ebc1231782b31ba0cc86c385eabdbf0f32ddf6
safwan <120546037+safwan994@users.noreply.github.com>
2022-12-19 15:41:54 +0800

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -16,4 +16,21 @@ alert("You have entered an invalid email address!");
 document.form1.text1.focus();
 return false;
 }
+  $to = 'yourmail@yourdomain.ltd';
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+$from = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_SPECIAL_CHARS);
+$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
+
+if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
+    $headers = ['From' => ($name?"<$name> ":'').$from,
+            'X-Mailer' => 'PHP/' . phpversion()
+           ];
+
+    mail($to, $subject, $message."\r\n\r\nfrom: ".$_SERVER['REMOTE_ADDR'], $headers);
+    die('OK');
+    
+} else {
+    die('Invalid address');
+}
 })();
