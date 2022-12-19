c729131e38dbd93751b4d64744c541935c88e2f6
safwan <120546037+safwan994@users.noreply.github.com>
2022-12-19 14:45:39 +0800

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,9 +1,8 @@
 <?php
   /**
   * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
+  * The "PHP Email Form" library is available 
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
   // Replace contact@example.com with your real receiving email address
@@ -22,6 +21,7 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
+
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
